26cae396c756758a154ef0eafa1f39e7d9703d5d
Elisee-des <esabidani@gmail.com>
2022-07-23 22:06:58 +0000

    Ok pour le changement de page page de facon dynamique

--- js/app.js
+++ js/app.js
@@ -1,10 +1,49 @@
-
 const App = {
-    data(){
-        return {
-            message : "Ca marche"
-        }
-    }
-}
+  data() {
+    return {
+      showHome: false,
+      showCreateFrom: false,
+      showStudentsList: false,
+    };
+  },
 
-Vue.createApp(App).mount("#app")
+  mounted() {
+    this.changeNavigationState("home");
+  },
+
+  methods: {
+    goToHome() {
+      this.changeNavigationState("home");
+    },
+    goToCreateFrom() {
+      this.changeNavigationState("create");
+    },
+    goToStudentsList() {
+      this.changeNavigationState("list");
+    },
+
+    changeNavigationState(destination) {
+      this.showHome = false;
+      this.showCreateFrom = false;
+      this.showStudentsList = false;
+
+      switch (destination) {
+        case "home":
+          this.showHome = true;
+          break;
+        case "create":
+          this.showCreateFrom = true;
+          break;
+        case "list":
+          this.showStudentsList = true;
+          break;
+
+        default:
+          this.showHome = true;
+          break;
+      }
+    },
+  },
+};
+
+Vue.createApp(App).mount("#app");
